fbfee23aefa7b95faaa0be68e7c414227d7f2c77
Saeed Amiri <samiri@uni-bremen.de>
2023-05-23 13:35:55 +0200

    main

---
+++ codes/main.py
@@ -0,0 +1,6 @@
+"""This script is designed for the analysis of simulation trajectories.
+It primarily requires the trajectory files, along with the corresponding
+topology and index files. Additionally, it can utilize the gro file,
+which represents the final state of the system. These input files enable
+the script to extract relevant data and perform various analyses on the
+simulation trajectories."""
