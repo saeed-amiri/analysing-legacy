7562990b2d1f9a3e9cf6db93b93093858b2b19a9
Saeed Amiri <samiri@uni-bremen.de>
2023-05-23 15:03:03 +0200

    prepare the log file

--- codes/logger.py
+++ codes/logger.py
@@ -2,6 +2,19 @@
 
 import logging
 import os
+import re
+
+
+# check if the log file is exist rename the new file
+log_files = [file for file in os.listdir('.') if
+             os.path.isfile(file) and file.startswith('log') and
+             re.match(r'log.\d+', file)]
+
+COUNT = 1
+if len(log_files) > 0:
+    COUNT = max(int(re.search(r'log.(\d+)', file).group(1)) for
+                file in log_files) + 1 if log_files else 1
+log_file = f"log.{COUNT}"
 
 
 def setup_logger():
@@ -16,7 +29,7 @@ def setup_logger():
     logger.setLevel(logging.DEBUG)
 
     # Create a file handler to write log messages to a file
-    file_handler = logging.FileHandler('app.log')
+    file_handler = logging.FileHandler(log_file)
     file_handler.setLevel(logging.DEBUG)
 
     # Define the log message format
