1a010893ffafcbe9f134cf4f68af21f90b184d8c
Saeed Amiri <samiri@uni-bremen.de>
2023-05-23 16:13:18 +0200

    clean up

--- codes/get_trajectory.py
+++ codes/get_trajectory.py
@@ -4,24 +4,32 @@ frames, and atoms. """
 
 import os
 import sys
+import logger
+from colors_text import TextColor as bcolors
 
 
 class GetInfo:
     """read trr files and return info from it"""
     def __init__(self,
-                 fname: str  # Name of the trr file
+                 fname: str,  # Name of the trr file
+                 log: logger.logging.Logger  # Name of the log file
                  ) -> None:
         if not self.path_esist(fname):
             sys.exit('No such file')
         self.trr: str = fname
-        self.__get_gro()
+        self.__get_gro(log)
 
-    def __get_gro(self) -> str:
+    def __get_gro(self,
+                  log: logger.logging.Logger  # Name of the log file
+                  ) -> str:
         """return the name of the gro file and check if it exist"""
         tmp: str = self.trr.split('.')[0]
         gro_file: str = f'{tmp}.gro'
-        if self.path_esist(gro_file):
-            print('gro dosenot exist')
+        if not self.path_esist(gro_file):
+            log.error(f'Error! `{gro_file}` dose not exist')
+            sys.exit(f'{bcolors.FAIL}{self.__class__.__name__}: '
+                     f'({self.__module__})\n Error! `{gro_file}` dose not '
+                     f'exist \n{bcolors.ENDC}')
         return gro_file
 
     def path_esist(self,
@@ -32,4 +40,4 @@ class GetInfo:
 
 
 if __name__ == '__main__':
-    trr = GetInfo(sys.argv[1])
+    trr = GetInfo(sys.argv[1], log=logger.setup_logger('test_log'))
