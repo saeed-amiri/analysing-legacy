ccc3e7cc62b47e7d5b876dab563eccb43edb36fe
Saeed Amiri <samiri@uni-bremen.de>
2023-05-24 12:44:07 +0200

    update

--- codes/main.py
+++ codes/main.py
@@ -4,3 +4,12 @@ topology and index files. Additionally, it can utilize the gro file,
 which represents the final state of the system. These input files enable
 the script to extract relevant data and perform various analyses on the
 simulation trajectories."""
+
+
+import sys
+import logger
+import get_trajectory as traj
+
+
+if __name__ == '__main__':
+    trr = traj.GetInfo(sys.argv[1], log=logger.setup_logger('test_log'))
