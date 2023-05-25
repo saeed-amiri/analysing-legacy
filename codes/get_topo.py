0e4586499985f2cfcdd934aadd4ba72c525444fa
Saeed Amiri <samiri@uni-bremen.de>
2023-05-25 10:21:08 +0200

    reading file

--- codes/get_topo.py
+++ codes/get_topo.py
@@ -12,9 +12,30 @@ from colors_text import TextColor as bcolors
 class ReadTop:
     """read the topology file;
     It should named: topol.top"""
+
     def __init__(self) -> None:
         self.fanme: str = 'topol.top'
         my_tools.check_file_exist(self.fanme, logger.setup_logger('test_log'))
+        self.get_top()
+
+    def get_top(self) -> None:
+        """read the top file"""
+        line: str  # Line from the file
+        endif: bool = False  # To pass the restrains parts
+        itp_dict: dict[str, str]  # To save the paths of itp files
+        with open(self.fanme, 'r', encoding='utf8') as f_r:
+            while True:
+                line = f_r.readline()
+                if line.strip():
+                    line = line.strip()
+                    if line.startswith(';'):
+                        pass
+                    elif line.startswith('#include') and not endif:
+                        pass
+                    else:
+                        print(line)
+                elif not line:
+                    break
 
 
 if __name__ == "__main__":
