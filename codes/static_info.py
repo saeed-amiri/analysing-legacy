c77e6a86081b20e209a41871078f4e04bc949f78
Saeed Amiri <samiri@uni-bremen.de>
2023-05-25 14:39:09 +0200

    update

--- codes/static_info.py
+++ codes/static_info.py
@@ -11,5 +11,34 @@ log = {
 # Constant values for reading topol file
 topo: dict[str, typing.Any]
 topo = {
-    'fname':  './topol.top',
+    'fname': './topol.top',
+}
+
+# Number of atoms in each residue make sure you update this for
+# different system size
+reidues_num: dict[str, int]
+reidues_num = {
+    'CLA': 1,
+    'SOL': 3,
+    'ODN': 59,
+    'D10': 32,
+    'APT_COR': 7924
+}
+
+# Giving id to each residues to not save str in array
+reidues_id: dict[str, int]
+reidues_id = {
+    'CLA': 1,
+    'SOL': 2,
+    'ODN': 3,
+    'D10': 4,
+    'APT_COR': 5
+}
+
+# Nano partcle
+np_info: dict[str, typing.Any]
+np_info = {
+    'np_residues': ['APT', 'COR'],
+    'np_name': 'APT_COR',
+    'solution_residues': ['CLA', 'SOL', 'ODN', 'D10']
 }
