d74132281b73855f38cbb42649365928e6957f01
Saeed Amiri <samiri@uni-bremen.de>
2023-05-25 12:22:50 +0200

    set the static info

---
+++ codes/static_info.py
@@ -0,0 +1,15 @@
+"""to save constant constants, paths, nams, which are used in the scripts"""
+import typing
+
+
+# logging files
+log: dict[str, typing.Any]
+log = {
+    'test_log': 'test_log'
+}
+
+# Constant values for reading topol file
+topo: dict[str, typing.Any]
+topo = {
+    'fname':  './topol.top',
+}
