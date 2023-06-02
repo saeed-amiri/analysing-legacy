1dc5ae7c3a80180dc59bca496de31e227811320b
Saeed Amiri <samiri@uni-bremen.de>
2023-06-02 10:46:59 +0200

    add func; clean up

--- codes/my_tools.py
+++ codes/my_tools.py
@@ -20,14 +20,37 @@ def check_file_exist(fname: str,  # Name of the file to check
         log.info(f'reading: `{fname}`')
 
 
+def check_file_reanme(fname: str,  # Name of the file to check
+                      log: logger.logging.Logger
+                      ) -> str:
+    """checking if the file fname is exist and if, rename the old one"""
+    # Check if the file already exists
+    if os.path.isfile(fname):
+        # Generate a new file name by appending a counter
+        counter = 1
+        while os.path.isfile(f"{fname}_{counter}"):
+            counter += 1
+        new_fname = f"{fname}_{counter}"
+
+        # Rename the existing file
+        os.rename(fname, new_fname)
+        print(f'{bcolors.CAUTION}{__name__}Renamed `{fname}` file to: '
+              f'`{new_fname}`{bcolors.ENDC}')
+        log.info(f'renmaing an old `{fname}` to `{new_fname}`')
+        return new_fname
+    return fname
+
+
 def drop_string(input_string: str,
                 string_to_drop: str
                 ) -> str:
+    """drop strings"""
     output_string = input_string.replace(string_to_drop, "")
     return output_string
 
 
 def extract_string(input_string: str) -> list[typing.Any]:
+    """return matches str"""
     pattern = r'"(.*?)"'
     matches = re.findall(pattern, input_string)
     return matches
