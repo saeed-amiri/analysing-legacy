9973fcb81b071cd490586f110876d9fc8f9e2b2f
Saeed Amiri <samiri@uni-bremen.de>
2023-06-02 10:20:32 +0200

    doc

--- codes/read_com.py
+++ codes/read_com.py
@@ -1,8 +1,20 @@
 """After running get_frames.py, the script reads the pickle file and
 stores its contents in an array. This data will then be used for both
-plotting and analyzing purposes."""
+plotting and analyzing purposes.
+The file has the following format:
+elements:
+The center of the mass of all residue is calculated, then it is wrapped
+back into the box, and the center of mass of the NP at that time is
+subtracted from it.
+Number of columns is 3(N+1) + 1
+    N: number of the residues,
+    and one for the center of mass of the NP at the time
+    and last column for saving the label of each residue
+"""
 
 
+import pickle
+import numpy as np
 import static_info as stinfo
 
 
@@ -11,7 +23,15 @@ class ReadCom:
     """
     def __init__(self) -> None:
         self.f_name: str = stinfo.files['com_pickle']
-        print(self.f_name)
+        self.com_arr: np.ndarray = self.__get_data()
+        print(self.com_arr[:, 0])
+
+    def __get_data(self) -> np.ndarray:
+        """reading the file"""
+        with open(self.f_name, 'rb') as f_rb:
+            com_arr = pickle.load(f_rb)
+        return com_arr
+    
 
 
 if __name__ == '__main__':
