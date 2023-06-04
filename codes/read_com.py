62afc37d7007186c28067e5bdc1cd8928c85def2
Saeed Amiri <samiri@uni-bremen.de>
2023-06-04 14:39:17 +0200

    get the x and y values

--- codes/read_com.py
+++ codes/read_com.py
@@ -9,13 +9,14 @@ subtracted from it.
 Number of columns is 3(N+1) + 1
     N: number of the residues,
     and one for the center of mass of the NP at the time
-    and last column for saving the label of each residue
+    and last row for saving the label of each residue
 """
 
 
 import pickle
 import numpy as np
 import static_info as stinfo
+import matplotlib.pylab as plt
 
 
 class ReadCom:
@@ -24,12 +25,29 @@ class ReadCom:
     def __init__(self) -> None:
         self.f_name: str = stinfo.files['com_pickle']
         self.com_arr: np.ndarray = self.__get_data()
+        self.__plot_com()
 
     def __get_data(self) -> np.ndarray:
         """reading the file"""
         with open(self.f_name, 'rb') as f_rb:
             com_arr = pickle.load(f_rb)
         return com_arr
+
+    def __plot_com(self) -> None:
+        """test plotting"""
+        for mol in ['ODN']:
+            column_x_indices = np.where(self.com_arr[-1] ==
+                                        stinfo.reidues_id[mol])
+            oda: np.ndarray = np.squeeze(self.com_arr[:, column_x_indices])
+            x_indices = range(3, oda.shape[1], 3)  # Indices on the x-axis
+            y_indices = range(4, oda.shape[1], 3)  # Indices on the y-axis
+            for i in range(12):
+                x_data = oda[i, x_indices]
+                y_data = oda[i, y_indices]
+                plt.scatter(x_data, y_data)
+        # Set the aspect ratio to 'equal'
+        plt.gca().set_aspect('equal')
+        plt.show()
 
 
 if __name__ == '__main__':
