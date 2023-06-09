0860927ac91dfab05d1917b47bc61a226d80d8b6
Saeed Amiri <samiri@uni-bremen.de>
2023-06-09 10:53:00 +0200

    initiate parallelization

--- codes/get_frames.py
+++ codes/get_frames.py
@@ -25,12 +25,18 @@ subtracted from it.
 import sys
 import pickle
 import numpy as np
+from mpi4py import MPI
 import logger
 import MDAnalysis as mda
 import static_info as stinfo
 import my_tools
 import get_topo as topo
 from get_trajectory import GetInfo
+
+
+COMM: MPI.Intracomm = MPI.COMM_WORLD  # MPI communicator object
+RANK: int = COMM.Get_rank()  # Total number of processes in the communicator
+SIZE: int = COMM.Get_size()  # Represents the rank (ID) of the current process
 
 
 class ResiduePositions:
@@ -48,8 +54,10 @@ class ResiduePositions:
                            ) -> None:
         """calculate the center mass of the each residue"""
         # update the residues index to get the NP: APT_COR
-        sol_residues: dict[str, list[int]] = self.__solution_residues()
-        com_arr: np.ndarray = self.__allocate(sol_residues)
+        if RANK == 0:
+            sol_residues: dict[str, list[int]] = self.__solution_residues()
+            com_arr: np.ndarray = self.__allocate(sol_residues)
+        COMM.Barrier()  # Wait for all processes to synchronize
         self.pickle_arr(com_arr, sol_residues, log)
 
     def pickle_arr(self,
@@ -59,10 +67,14 @@ class ResiduePositions:
                    ) -> None:
         """check the if the previus similar file exsitance the pickle
         data into a file"""
-        fname: str  # Name of the file to pickle to
-        fname = my_tools.check_file_reanme(stinfo.files['com_pickle'], log)
-        with open(fname, 'wb') as f_arr:
-            pickle.dump(self.__get_coms(com_arr, sol_residues), f_arr)
+        if RANK == 0:
+            fname: str  # Name of the file to pickle to
+            fname = my_tools.check_file_reanme(stinfo.files['com_pickle'], log)
+        _com_arr = self.__get_coms(com_arr, sol_residues)
+        COMM.Barrier()  # Wait for all processes to synchronize
+        if RANK == 0:
+            with open(fname, 'wb') as f_arr:
+                pickle.dump(_com_arr, f_arr)
 
     def __get_coms(self,
                    com_arr: np.ndarray,  # Zero array to save the coms
