fb0ebb243cd85eb4e356c54ec67e3d2f40bb9aa6
Saeed Amiri <samiri@uni-bremen.de>
2023-07-14 15:43:25 +0200

    get solution residues

--- codes/get_frame_mpi.py
+++ codes/get_frame_mpi.py
@@ -23,10 +23,9 @@ and  the number of rows will be the number of timeframes
 
 
 import sys
+import json
 import logger
-import MDAnalysis as mda
 import static_info as stinfo
-import my_tools
 import get_topo as topo
 from get_trajectory import GetInfo
 
@@ -38,12 +37,16 @@ class GetResiduesPosition:
     """
 
     info_msg: str = 'Messages:\n'  # To log
+    # The following will set in _initiate_reading
+    top: topo  # Topology file
+    trr_info: GetInfo  # All the info from trajectory
 
     def __init__(self,
                  fname: str,  # Name of the trajectory file
                  log: logger.logging.Logger
                  ) -> None:
         self._initiate_reading(fname, log)
+        self._initiate_data(log)
 
     def _initiate_reading(self,
                           fname: str,  # Name of the trajectory file
@@ -55,6 +58,25 @@ class GetResiduesPosition:
         self.top = topo.ReadTop(log)
         self.trr_info = GetInfo(fname, log=log)
 
+    def _initiate_data(self) -> None:
+        """
+        Initiate setting data to get the COM of the residues
+        """
+        self.get_solution_residues()
+
+    def get_solution_residues(self) -> dict[str, list[int]]:
+        """
+        Return the dict of the residues in the solution with
+        dropping the NP residues
+        """
+        self.info_msg += '\tGetting the residues in Solution:\n'
+        self.info_msg += \
+            f'\t{json.dumps(stinfo.np_info["solution_residues"], indent=8)}'
+        sol_dict: dict[str, list[int]] = {}  # All the residues in solution
+        for k, val in self.trr_info.residues_indx.items():
+            if k in stinfo.np_info['solution_residues']:
+                sol_dict[k] = val
+        return sol_dict
 
 if __name__ == '__main__':
     GetResiduesPosition(fname=sys.argv[1],
