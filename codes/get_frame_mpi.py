717fb1a2679514782908862fbae926aae5c6a071
Saeed Amiri <samiri@uni-bremen.de>
2023-07-14 15:27:42 +0200

    initiate reading

--- codes/get_frame_mpi.py
+++ codes/get_frame_mpi.py
@@ -20,3 +20,42 @@ timeframe + NP_com + n_residues: xyz,type + n_oda * xyz,type
 and  the number of rows will be the number of timeframes
 
 """
+
+
+import sys
+import logger
+import MDAnalysis as mda
+import static_info as stinfo
+import my_tools
+import get_topo as topo
+from get_trajectory import GetInfo
+
+
+class GetResiduesPosition:
+    """
+    Get the positions of the residues, based on the description on the
+    doc.
+    """
+
+    info_msg: str = 'Messages:\n'  # To log
+
+    def __init__(self,
+                 fname: str,  # Name of the trajectory file
+                 log: logger.logging.Logger
+                 ) -> None:
+        self._initiate_reading(fname, log)
+
+    def _initiate_reading(self,
+                          fname: str,  # Name of the trajectory file
+                          log: logger.logging.Logger
+                          ) -> None:
+        """
+        Call the other modules and read the files
+        """
+        self.top = topo.ReadTop(log)
+        self.trr_info = GetInfo(fname, log=log)
+
+
+if __name__ == '__main__':
+    GetResiduesPosition(fname=sys.argv[1],
+                        log=logger.setup_logger('get_frames_mpi_log'))
