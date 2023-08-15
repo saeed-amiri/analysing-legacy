90b952af342f155518264e65b37d31ae2f010c55
Saeed Amiri <samiri@uni-bremen.de>
2023-08-15 14:49:14 +0200

    add functions for canvas

--- codes/plot_tools.py
+++ codes/plot_tools.py
@@ -1,7 +1,9 @@
 """tools for ploting"""
 
+import numpy as np
 import matplotlib
 import matplotlib.pylab as plt
+import static_info as stinfo
 
 
 def set_sizes(width: float,  # Width of the plot in points
@@ -56,3 +58,144 @@ def mk_circle(radius: float,
                         linestyle='dashed',
                         fill=False, alpha=1)
     return circle
+
+def mk_canvas(x_range: tuple[float, ...],
+              num_xticks
+              ) -> tuple[plt.figure, plt.axes]:
+    """
+    Create a canvas for the plot.
+
+    This method generates a canvas (figure and axes) for plotting.
+
+    Args:
+        x_range (tuple[float, ...]): Range of x-axis values.
+        num_xticks (int, optional): Number of x-axis ticks.
+        Default is 5.
+
+    Returns:
+        tuple[plt.figure, plt.axes]: A tuple containing the figure
+        and axes objects.
+    """
+    width = stinfo.plot['width']
+    fig_main, ax_main = \
+        plt.subplots(1, figsize=set_sizes(width))
+    # Set font for all elements in the plot)
+    xticks = np.linspace(x_range[0], x_range[-1], num_xticks)
+    ax_main.set_xticks(xticks)
+    ax_main = set_x2ticks(ax_main)
+    ax_main = set_ax_font_label(ax_main)
+    return fig_main, ax_main
+
+def save_close_fig(fig: plt.figure,  # The figure to save,
+                   axs: plt.axes,  # Axes to plot
+                   fname: str,  # Name of the output for the fig
+                   loc: str = 'upper right',  # Location of the legend
+                   transparent=False
+                   ) -> None:
+    """
+    Save the figure and close it.
+
+    This method saves the given figure and closes it after saving.
+
+    Args:
+        fig (plt.figure): The figure to save.
+        axs (plt.axes): The axes to plot.
+        fname (str): Name of the output file for the figure.
+        loc (str, optional): Location of the legend. Default is
+        'upper right'.
+    """
+    legend = axs.legend(loc=loc, bbox_to_anchor=(1.0, 1.0))
+    legend.set_bbox_to_anchor((1.0, 1.0))
+    fig.savefig(fname,
+                dpi=300,
+                pad_inches=0.1,
+                edgecolor='auto',
+                bbox_inches='tight',
+                transparent=transparent
+                )
+    plt.close(fig)
+
+def set_x2ticks(ax_main: plt.axes  # The axes to wrok with
+                    ) -> plt.axes:
+    """
+    Set secondary x-axis ticks.
+
+    This method sets secondary x-axis ticks for the given main axes.
+
+    Args:
+        ax_main (plt.axes): The main axes to work with.
+
+    Returns:
+        plt.axes: The modified main axes.
+    """
+    ax_main.tick_params(axis='both', direction='in')
+    # Set twiny
+    ax2 = ax_main.twiny()
+    ax2.set_xlim(ax_main.get_xlim())
+    # Synchronize x-axis limits and tick positions
+    ax2.xaxis.set_major_locator(ax_main.xaxis.get_major_locator())
+    ax2.xaxis.set_minor_locator(ax_main.xaxis.get_minor_locator())
+    ax2.set_xticklabels([])  # Remove the tick labels on the top x-axis
+    ax2.tick_params(axis='x', direction='in')
+    for ax_i in [ax_main, ax2]:
+        ax_i.xaxis.set_major_locator(matplotlib.ticker.AutoLocator())
+        ax_i.xaxis.set_minor_locator(
+            matplotlib.ticker.AutoMinorLocator(n=5))
+        ax_i.tick_params(which='minor', direction='in')
+    return ax_main
+
+def set_y2ticks(ax_main: plt.axes  # The axes to wrok with
+                    ) -> plt.axes:
+    """
+    Set secondary y-axis ticks.
+
+    This method sets secondary y-axis ticks for the given main axes.
+
+    Args:
+        ax_main (plt.axes): The main axes to work with.
+
+    Returns:
+        plt.axes: The modified main axes.
+    """
+    # Reset the y-axis ticks and locators
+    ax3 = ax_main.twinx()
+    ax3.set_ylim(ax_main.get_ylim())
+    # Synchronize y-axis limits and tick positions
+    ax3.yaxis.set_major_locator(ax_main.yaxis.get_major_locator())
+    ax3.yaxis.set_minor_locator(ax_main.yaxis.get_minor_locator())
+    ax3.set_yticklabels([])  # Remove the tick labels on the right y-axis
+    ax3.tick_params(axis='y', direction='in')
+    for ax_i in [ax_main, ax3]:
+        ax_i.yaxis.set_major_locator(matplotlib.ticker.AutoLocator())
+        ax_i.yaxis.set_minor_locator(
+            matplotlib.ticker.AutoMinorLocator(n=5))
+        ax_i.tick_params(which='minor', direction='in')
+    return ax_main
+
+def set_ax_font_label(ax_main: plt.axes,  # Main axis to set parameters
+                      fsize: int = 0  # font size if called with font
+                      ) -> plt.axes:
+    """
+    Set font and labels for the plot axes.
+
+    This method sets font size and labels for the plot axes.
+
+    Args:
+        ax_main (plt.axes): The main axis to set parameters for.
+        fsize (int, optional): Font size if called with font.
+        Default is 0.
+
+    Returns:
+        plt.axes: The modified main axis.
+    """
+    if fsize == 0:
+        fontsize = 14
+        ax_main.set_xlabel('frame index', fontsize=fontsize)
+        ax_main.set_ylabel('z [A]', fontsize=fontsize)
+    else:
+        fontsize = fsize
+    matplotlib.rcParams['font.family'] = 'sans-serif'
+    matplotlib.rcParams['font.size'] = fontsize
+    ax_main.tick_params(axis='x', labelsize=fontsize)
+    ax_main.tick_params(axis='y', labelsize=fontsize)
+    return ax_main
