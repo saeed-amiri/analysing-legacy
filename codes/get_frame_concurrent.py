8cf5db943b1d22f59dde79139844c75e7809ea6c
Saeed Amiri <samiri@uni-bremen.de>
2023-09-12 14:11:35 +0200

    initiate

---
+++ codes/get_frame_concurrent.py
@@ -0,0 +1,10 @@
+#! /home/saeed/.conda/envs/analysing/bin/python
+"""
+Using multiprocessing to get the COM of the residues!
+"""
+# Import the required Python module
+from trajectory_residue_extractor import GetResidues
+
+
+if __name__ == '__main__':
+    pass
