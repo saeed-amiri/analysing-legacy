df3f8bda28a6b3f4a1047a1bc426b811782deefe
Saeed Amiri <samiri@uni-bremen.de>
2023-09-18 11:13:10 +0200

    get wrapdata

--- codes/water_analysing.py
+++ codes/water_analysing.py
@@ -3,5 +3,54 @@ Analysing water residues
 1- Finding the surface position of the water at the interface
 """
 
+import numpy as np
+
+from get_data import GetData
+import static_info as stinfo
+
+
+class WrapData(GetData):
+    """
+    Get data and call other classes to analysis and plot them.
+    Before that, some calculateion must be done
+    """
+
+    mean_nanop_com: np.ndarray  # Average of the nanoparticle COM over times
+    # Shift of COM at each time from average
+    nanoparticle_disp_from_avg_com: np.ndarray
+    interface_locz: float  # Z location of the interface
+    nanop_radius: float  # Radius of the nanoparticle
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.set_constants()
+        self._initiate_calc()
+
+    def set_constants(self) -> None:
+        """
+        Set the constants for all the uses
+        """
+        self.interface_locz = 113.9
+        self.nanop_radius = stinfo.np_info['radius']
+
+    def _initiate_calc(self) -> None:
+        """
+        Initiate calculation and some analysis which are needed for
+        all other classes
+        """
+        self.mean_nanop_com = self.find_mean_of_np_com()
+        self.nanoparticle_disp_from_avg_com = self.find_np_shift_from_mean()
+
+    def find_mean_of_np_com(self) -> np.ndarray:
+        """find mean of the nanoparticle center of mass"""
+        return np.mean(self.split_arr_dict['APT_COR'], axis=0)
+
+    def find_np_shift_from_mean(self,
+                                ) -> np.ndarray:
+        """find the shift of com of nanoparticle from mean value at
+        each time step"""
+        return self.split_arr_dict['APT_COR'] - self.mean_nanop_com
+
+
 if __name__ == "__main__":
     pass
